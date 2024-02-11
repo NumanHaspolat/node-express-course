2803b0c15d384c40c69b0878f0bb8cf464677e24
Numan Haspolat <haspolatnuman@gmail.com>
2024-02-11 21:16:48 +0300

    5:03

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,30 @@
 const express = require("express");
 const app = express();
+
+//* app.get
+//* app.post
+//* app.put
+//* app.delete
+//* app.all
+//* app.use
+//* app.listen
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page(express)");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page(express)");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send(" <h1>Resource not found</h1> ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listeningon port ${port}...`);
+});
+
+//TODO 5:03
